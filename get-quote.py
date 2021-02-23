9f32e280359bf0ae2e528917b54ed029bf7353de
Yogendra31S <79274405+Yogendra31S@users.noreply.github.com>
2021-02-22 22:34:58 -0800

    Add files via upload

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,16 @@
-def plain():
+import random   
+def main():
   #print("Keep it logically awesome.")   
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  last = len(quotes)- 1 
   f.close()
 
-  print(quotes[13])
+     
+  rnd = random.randint(0, last) 
+  print(quotes[rnd])
 
 if __name__== "__main__":
-  plain()
+  main()
 
